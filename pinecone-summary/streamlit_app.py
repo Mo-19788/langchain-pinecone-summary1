f37f158bc16362560ced3ef90ef50bf6b32f7c90
Mo-19788 <139487583+Mo-19788@users.noreply.github.com>
2023-07-13 18:09:51 +0100

    Update streamlit_app.py
    included gpt-4

--- pinecone-summary/streamlit_app.py
+++ pinecone-summary/streamlit_app.py
@@ -36,10 +36,10 @@ if st.button("Summarize"):
             vectordb = Pinecone.from_documents(pages, embeddings, index_name=pinecone_index)
 
             # Initialize the OpenAI module, load and run the summarize chain
-            llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
+            llm = OpenAI(model='gpt-4', temperature=0, openai_api_key=openai_api_key)
             chain = load_summarize_chain(llm, chain_type="stuff")
             search = vectordb.similarity_search(" ")
-            summary = chain.run(input_documents=search, question="Write a concise summary within 200 words.")
+            summary = chain.run(input_documents=search, question="Write a concise summary within 400 words.")
             
             st.success(summary)
         except Exception as e:
